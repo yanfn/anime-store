b95959f8323f756fd9184048e02e80c5e834fe58
nairbflix <grimhwok@gmail.com>
2023-04-05 16:41:52 +0800

    get request for sellers

--- api/admin/function.php
+++ api/admin/function.php
@@ -44,6 +44,50 @@ function getCustomerList () {
 }
 
 
+function getSellerList () {
+    global $conn;
+
+    $query = "SELECT * FROM sellers";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+
+        if (mysqli_num_rows($result) > 0) {
+
+            $response = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+            $data = [
+                'status' => 200,
+                'message' => 'Seller List Fetch Successfully'
+                'data' => $response
+            ];
+            http_response_code(200);
+            echo json_encode($data);
+            
+        }
+        else {
+            
+            $data = [
+                'status' => 404,
+                'message' => 'No Sellers Found'
+            ];
+            http_response_code(404);
+            echo json_encode($data);
+        }
+    }
+    else {
+
+        $data = [
+            'status' => 500,
+            'message' => 'Internal Server Error'
+        ];
+        http_response_code(500);
+        echo json_encode($data);
+    }
+
+
+}
+
 
 
 ?>
