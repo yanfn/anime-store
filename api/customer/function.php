b2f70fe1cf0eacdba938f834ed0d243febf28ebd
nairbflix <grimhwok@gmail.com>
2023-05-12 20:37:23 +0800

    adding category and editing functions

--- api/customer/function.php
+++ api/customer/function.php
@@ -8,7 +8,7 @@ function error422 ($message){
         'message' => $message,
     ];
     http_response_code(422);
-    echo json_encode($data);
+    return json_encode($data);
     exit();
 }
 
@@ -16,7 +16,7 @@ function storeCustomer ($customerInput) {
     
     global $conn;
 
-    $type = 'customer';
+    $role = 'customer';
     $email = mysqli_real_escape_string($conn, $customerInput['email']);
     $password = mysqli_real_escape_string($conn, $customerInput['password']);
     $fname = mysqli_real_escape_string($conn, $customerInput['fname']);
@@ -24,12 +24,13 @@ function storeCustomer ($customerInput) {
     $sex = mysqli_real_escape_string($conn, $customerInput['sex']);
     $raw_date = mysqli_real_escape_string($conn, $customerInput['date']);
     $phone = mysqli_real_escape_string($conn, $customerInput['phone']);
-    $street = mysqli_real_escape_string($conn, $customerInput['street']);
-    $city = mysqli_real_escape_string($conn, $customerInput['city']);
+    $region = mysqli_real_escape_string($conn, $customerInput['region']);
     $province = mysqli_real_escape_string($conn, $customerInput['province']);
+    $city = mysqli_real_escape_string($conn, $customerInput['city']);
+    $barangay = mysqli_real_escape_string($conn, $customerInput['barangay']);
     $zipcode = mysqli_real_escape_string($conn, $customerInput['zipcode']);
     
-    $date = date('Y-m-d', strtotime($raw_date));
+    $birthdate = date('Y-m-d', strtotime($raw_date));
 
     if (empty(trim($email))) {
         return error422('Enter your email');
@@ -41,75 +42,68 @@ function storeCustomer ($customerInput) {
         return error422('Enter your last name');
     }elseif (empty(trim($sex))) {
         return error422('Enter your sex');
-    }elseif (empty(trim($date))) {
-        return error422('Enter your date');
+    }elseif (empty(trim($birthdate))) {
+        return error422('Enter your birthdate');
     }elseif (empty(trim($phone))) {
         return error422('Enter your phone');
-    }elseif (empty(trim($street))) {
-        return error422('Enter your street');
-    }elseif (empty(trim($city))) {
-        return error422('Enter your city');
+    }elseif (empty(trim($region))) {
+        return error422('Enter your region');
     }elseif (empty(trim($province))) {
         return error422('Enter your province');
+    }elseif (empty(trim($city))) {
+        return error422('Enter your city');
+    }elseif (empty(trim($barangay))) {
+        return error422('Enter your barangay');
     }elseif (empty(trim($zipcode))) {
         return error422('Enter your zipcode');
     }
     else {
-        $user_query = "INSERT INTO users (email, password, type) VALUES ('$email', '$password', '$type')";
+        $user_query = "INSERT INTO users (email, password, fname, lname, sex, birthdate, role) VALUES ('$email', '$password', '$fname', '$lname', '$sex', '$birthdate', '$role')";
         $user_result = mysqli_query($conn, $user_query);
 
-        if ($user_result) {
 
-            $user_id = mysqli_insert_id($conn);
+           if ($user_result) {
 
-            $customer_query = "INSERT INTO customers (user_id, fname, lname, sex, birthdate, phone_number) VALUES ('$user_id', '$fname', '$lname', '$sex', '$date', '$phone')";
-            $customer_result = mysqli_query($conn, $customer_query);
+            $customer_id = mysqli_insert_id($conn);
 
-            if ($customer_result) {
+            $address_query = "INSERT INTO customer_address (customer_id, phone_number, region, province, city, barangay, zip_code) VALUES ('$customer_id', '$phone', '$region', '$province', '$city', '$barangay', '$zipcode')";
+            $address_result = mysqli_query($conn, $address_query);
 
-                $customer_id = mysqli_insert_id($conn);
-
-                $address_query = "INSERT INTO customer_address (customer_id, street_address, city, province, zip_code) VALUES ('$customer_id', '$street', '$city', '$province', '$zipcode')";
-                $address_result = mysqli_query($conn, $address_query);
-
-                if ($address_result) {
-                    $data = [
-                        'status' => 201,
-                        'message' => 'Account created successfully',
-                        'customer' => [
-                            'id' => $customer_id,
-                            'fname' => $fname,
-                            'lname' => $lname,
-                            'sex' => $sex,
-                            'birthdate' => $date,
-                            'phone_number' => $phone,
-                            'address' => [
-                                'street_address' => $street,
-                                'city' => $city,
-                                'province' => $province,
-                                'zip_code' => $zipcode
-                            ]
+            if ($address_result) {
+                $data = [
+                    'status' => 201,
+                    'message' => 'Account created successfully',
+                    'customer' => [
+                        'id' => $customer_id,
+                        'fname' => $fname,
+                        'lname' => $lname,
+                        'sex' => $sex,
+                        'birthdate' => $birthdate,
+                        'phone_number' => $phone,
+                        'address' => [
+                            'phone' => $phone,
+                            'region' => $region,
+                            'province' => $province,
+                            'city' => $city,
+                            'barangay' => $barangay,
+                            'zip_code' => $zipcode
                         ]
-                    ];
-                    http_response_code(201);
-                    return json_encode($data);
-                }
-                else {
-                    $data = [
-                        'status' => 500,
-                        'message' => 'Internal server error'
-                    ];
-                    http_response_code(500);
-                    return json_encode($data);
-                }
+                    ]
+                ];
+                http_response_code(201);
+                return json_encode($data);
+            }
+            else {
+                $data = [
+                    'status' => 500,
+                    'message' => 'Internal server error'
+                ];
+                http_response_code(500);
+                return json_encode($data);
             }
         }
+        
     }
-
-
-
-
 }
-
 
 ?>
